f8d17875072543b38f449cc542bd6611948557f2
Deepankar Suman <deepankarsuman16@gmail.com>
2026-06-08 02:34:17 +0530

    debug: add detailed token logging to status route

--- app/api/spotify/status/route.ts
+++ app/api/spotify/status/route.ts
@@ -3,16 +3,20 @@ import { cookies } from 'next/headers'
 
 export async function GET() {
   const cookieStore = await cookies()
-  const hasAccessToken = cookieStore.has('spotify_access_token')
-  const hasRefreshToken = cookieStore.has('spotify_refresh_token')
+  const accessToken = cookieStore.get('spotify_access_token')?.value
+  const refreshToken = cookieStore.get('spotify_refresh_token')?.value
   
   console.log('Spotify Status Check:', {
-    hasAccessToken,
-    hasRefreshToken,
-    allCookies: cookieStore.getAll().map(c => c.name)
+    hasAccessToken: !!accessToken,
+    accessTokenPrefix: accessToken ? accessToken.substring(0, 10) + '...' : 'none',
+    hasRefreshToken: !!refreshToken,
+    refreshTokenPrefix: refreshToken ? refreshToken.substring(0, 10) + '...' : 'none',
+    allCookieNames: cookieStore.getAll().map(c => c.name)
   })
 
   return NextResponse.json({
-    connected: hasAccessToken || hasRefreshToken
+    connected: !!(accessToken || refreshToken),
+    hasAccessToken: !!accessToken,
+    hasRefreshToken: !!refreshToken
   })
 }
